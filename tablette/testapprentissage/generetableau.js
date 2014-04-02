2d28151bf4c04ebcde38388ebdc7f27a2999bdb3
phi <phi@phi-laptop>
2014-04-02 17:56:56 +0200

    Ajout des symboles. Couleurs définies dans une table associative.

--- tablette/testapprentissage/generetableau.js
+++ tablette/testapprentissage/generetableau.js
@@ -1,18 +1,15 @@
-var map = [
-["white", "red", "red", "white", "white", "red", "red", "white", "white", "red", "red", "white"],
-["white", "red", "red", "white", "blue", "blue", "blue", "blue", "white", "red", "red", "white"],
-["white", "red", "blue", "blue", "white", "red", "red", "white", "blue", "blue", "red", "white"],
-["white", "red", "blue", "white", "white", "red", "red", "white", "white", "blue", "red", "white"],
-["red", "blue", "white", "white", "blue", "red", "white", "blue", "red", "red", "blue", "white"],
-["red", "blue", "white", "white", "red", "red", "white", "white", "blue", "red", "blue", "white"],
-["red", "blue", "white", "white", "red", "red", "white", "white", "blue", "red", "blue", "white"],
-["red", "blue", "white", "white", "blue", "red", "white", "blue", "red", "red", "blue", "white"],
-["red", "white", "blue", "red", "red", "white", "white", "red", "red", "blue", "white", "red"],
-["red", "white", "blue", "blue", "red", "white", "white", "red", "blue", "blue", "white", "red"],
-["red", "white", "white", "red", "blue", "blue", "blue", "blue", "red", "white", "white", "red"],
-["red", "white", "white", "red", "red", "white", "white", "red", "red", "white", "white", "red"]];
+document.rootElement.addEventListener("SVGLoad", createMap, false);
 
-document.rootElement.addEventListener('SVGLoad', createMap, false);
+function symbolecircle(x,y,width,color)
+{
+  circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", x+width/2);
+  circle.setAttributeNS(null, "cy", y+width/2);
+  circle.setAttributeNS(null, "r", width/3);
+  circle.setAttributeNS(null, "style", "fill:"+color);
+  return circle;
+}
+
 
 function createMap()
 {
@@ -20,13 +17,34 @@ function createMap()
   {
     for (var j=0; j<12; j++)
     {
-      tile = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-      tile.setAttributeNS(null, 'x', 40*i);
-      tile.setAttributeNS(null, 'y', 40*j);
-      tile.setAttributeNS(null, 'width', 40);
-      tile.setAttributeNS(null, 'height', 40);
-      tile.setAttributeNS(null, 'style', "fill:"+map[i][j]+";stroke:black;stroke-width:1px;");
+      tile = document.createElementNS(svgns, "rect");
+      tile.setAttributeNS(null, "x", 40*i);
+      tile.setAttributeNS(null, "y", 40*j);
+      tile.setAttributeNS(null, "width", 40);
+      tile.setAttributeNS(null, "height", 40);
+      color = mapcolordefault;
+      if (background[i][j] in mapcolor)
+        color = mapcolor[background[i][j]];
+      tile.setAttributeNS(null, "style", "fill:"+color+";stroke:black;stroke-width:1px;");
       document.rootElement.appendChild(tile);
+    }
+  }
+
+  for (var i=0; i<12; i++)
+  {
+    for (var j=0; j<12; j++)
+    {
+      color = mapsymbcolordefault;
+      if (background[i][j] in mapsymbcolor)
+        color = mapsymbcolor[background[i][j]];
+
+      switch(symb[i][j])
+      {
+        case "C":
+          circle = symbolecircle(40*i,40*j,40, color);
+          document.rootElement.appendChild(circle);
+          break;
+      }
     }
   }
 }
