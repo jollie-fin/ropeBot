862fa89dfe9c54c79df2271faa4f7e1e969888af
phi <phi@phi-laptop>
2014-04-24 18:49:16 +0200

    déclenchement de l'interruption et de l'acquisition sur timer2.

--- robot/software/src/comm.c
+++ robot/software/src/comm.c
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAJORITY 5
-#define MAJORITY_THRES 3
+#define TIME_THRESHOLD 3
 #define BUFFER_SIZE 50
 
 #define ZERO 1
@@ -13,60 +12,93 @@
 #define HIZ 3
 #define UNKNOWN 0
 
-static uint8_t _majority[MAJORITY];
-static uint8_t _majority_index;
-static uint8_t _last_value;
-static uint8_t _threshold[4] = {12,30,50,70};
+static uint8_t _last_read_value;
+static uint8_t _last_valid_value;
+static uint8_t _last_seen_value;
+static uint8_t _consecutive;
+static uint8_t _value_threshold[4] = {10,30,50,70};
 static uint32_t _timestamp;
 static uint32_t _last_timestamp;
 static uint8_t _buffer[BUFFER_SIZE];
 static uint16_t _buffer_delay[BUFFER_SIZE];
 static uint8_t _buffer_index;
 
-void acquisition(uint8_t val)
+
+
+void C_init()
 {
+}
+
+inline void C_stop_interrupt()
+{
+  asm volatile("sts %0,__zero_reg__\n\t"::"M" (_SFR_MEM_ADDR(TIMSK2)));
+}
+
+inline void C_start_interrupt()
+{
+  asm volatile("sts %0,%1\n\t"::"M" (_SFR_MEM_ADDR(TIMSK2)), "r" ((uint8_t) _BV(OCIE2A)));
+}
+
+
+
+__attribute__((optimize("O3")))
+ISR(TIMER2_COMPA_vect)
+{
+  uint8_t adc = ADCL;
+
+  if (ADCSRA & _BV(ADSC))
+    adc = _last_read_value;
+  else
+  {
+    adc = ADCL;
+    ADCSRA |= ADSC; //start again a conversion
+    _last_read_value = adc;
+  }
+
   _timestamp++;
-  uint8_t val_bit = UNKNOWN;
-  if (val <= _threshold[0])
-    val_bit = ZERO;
-  else if (val >= _threshold[1] && val <= _threshold[2])
-    val_bit = HIZ;
-  else if (val >= _threshold[3])
-    val_bit = ONE;
+  uint8_t value_read = UNKNOWN;
+  if (adc <= _value_threshold[0])
+    value_read = ZERO;
+  else if (adc >= _value_threshold[1] && adc <= _value_threshold[2])
+    value_read = HIZ;
+  else if (adc >= _value_threshold[3])
+    value_read = ONE;
 
-  if (val_bit)
+  if (value_read != _last_seen_value)
   {
-    _majority[_majority_index] = val_bit;
-    _majority_index = (_majority_index+1)%MAJORITY;
+    _last_seen_value = value_read;
+    _consecutive = 0;
+    return;
   }
 
-  uint8_t rank[4] = {0, 0, 0, 0};
-  uint8_t i;
-  for (i = 0; i < MAJORITY; i++)
-  {
-    rank[_majority[i]]++;
-  }
-  if (rank[UNKNOWN])
+
+  if (!value_read)
+    return;
+
+  if (_consecutive == TIME_THRESHOLD)
+    return;
+
+  _consecutive++;
+  
+  if (_consecutive < TIME_THRESHOLD)
     return;
   
-  val_bit = _last_value;
-  for (i = 0; i <= 2; i++)
+  //this point, value_read correspond to the input value with strong guarantee
+  
+  if (value_read != _last_valid_value)
   {
-    if (rank[i] >= MAJORITY_THRES)
-      val_bit = i;
-  }
-  if (!val_bit)
-    return;
+    _last_valid_value = value_read;
+    _buffer[_buffer_index] = value_read;
 
-  if (val_bit != _last_value)
-  {
-    _buffer[_buffer_index] = val_bit;
-    _last_value = val_bit;
     uint32_t dt = _timestamp - _last_timestamp;
     if (dt > 65535)
       dt = 65535;
-
     _buffer_delay[_buffer_index] = dt;
+
+    _buffer_index++;
+    _buffer_index %= BUFFER_SIZE;
+
+
     _last_timestamp = _timestamp;
   }
 }
--- robot/software/src/comm.h
+++ robot/software/src/comm.h
@@ -1,3 +1,8 @@
 #ifndef COMM_H
 #define COMM_H
+
+void C_init(void);
+inline void C_stop_interrupt();
+inline void C_start_interrupt();
+
 #endif 
