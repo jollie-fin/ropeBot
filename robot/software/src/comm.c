18966b3d1340c817a2465642b43fb2bb9a42e85a
phi <phi@phi-laptop>
2014-04-28 10:41:41 +0200

    Début de réduction de la taille des interruptions : utilisation d'un buffer + assembleur

--- robot/software/src/comm.c
+++ robot/software/src/comm.c
@@ -8,19 +8,21 @@
 #include "comm.h"
 
 #define TIME_THRESHOLD 3
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 64 //(3.2ms at 20000smp/s)
 
 #define ZERO 0
 #define ONE 1
 #define HIZ 3
 #define UNKNOWN 2
 
+static volatile uint8_t _fifo_first;
+static volatile uint8_t _fifo_last;
+static uint8_t _buffer[BUFFER_SIZE];
 static uint8_t _last_read_value = UNKNOWN;
 static uint8_t _last_valid_value = UNKNOWN;
 static uint8_t _last_seen_value = UNKNOWN;
 static uint8_t _consecutive;
 static uint8_t _value_threshold[4] = {'2','3','5','6'};
-static uint32_t volatile _timestamp;
 static uint16_t _time_passed;
 static uint16_t _data;
 static uint8_t _no_bit;
@@ -35,19 +37,67 @@ void C_init()
 }
 
 
-
+#ifdef ADC_vect_C
 __attribute__((optimize("O3")))
-ISR(TIMER2_COMPA_vect)
+ISR(ADC_vect)
 {
-/*  uint8_t adc = ADCL;
-  if (ADCSRA & _BV(ADSC))
-    adc = _last_read_value;
-  else
-  {
-    adc = ADCL;
-    ADCSRA |= ADSC; //start again a conversion
-    _last_read_value = adc;
-  }*/
+  uint8_t next_fifo_last = (uint8_t) (_fifo_last+1)%BUFFER_SIZE;
+  if (next_fifo_last == _fifo_first)
+    return;
+  _fifo_last=next_fifo_last;   
+  _buffer[_fifo_last] = ADCL;
+}
+#else
+//36 cycles vs 50cycles
+ISR(ADC_vect, ISR_NAKED)
+{
+#if BUFFER_SIZE != 64
+#error "ISR(ADC_vect) was designed to work with BUFFER_SIZE == 64"
+#endif
+
+  asm volatile(
+	"push r0\n"
+  "/*save SREG*/\n\t"
+  "in r0,__SREG__\n\t"
+	"push r25\n\t"
+	"push r30\n"
+  "/*end of prologue*/\n\t"
+	"push r31\n\t"
+	"lds r30,_fifo_last\n"
+  "/*save _fifo_last for later*/\n\t"
+  "mov r31,r30\n"
+  "/*increment r31*/\n\t" 
+	"inc r31\n"
+  "/*modulo 64*/\n\t"
+	"andi r31,lo8(63)\n\t"
+	"lds r25,_fifo_first\n"
+  "/*if the fifo is full*/\n\t"
+	"cp r31,r25\n\t"
+	"breq 1f\n"
+  "/*save the new end of the fifo*/\n\t"
+  "sts _fifo_last,r31\n"
+  "/*read the ADC*/\n\t"
+	"lds r25,%0\n"
+  "/*compute _buffer[_fifo_last]*/\n\t"
+	"clr r31\n\t"
+	"subi r30,lo8(-(_buffer))\n\t"
+	"sbci r31,hi8(-(_buffer))\n"
+  "/*store the value read on ADC*/\n\t"
+	"st Z,r25\n"
+  "/*start of epilogue*/\n"
+  "1:\n\t"
+  "pop r31\n\t"
+	"pop r30\n\t"
+	"pop r25\n"
+  "/*restore SREG*/\n\t"
+	"out __SREG__,r0\n\t"
+	"pop r0\n\t"
+	"reti\n\t"::"M" (_SFR_MEM_ADDR(ADCL)));
+}
+#endif
+
+/*
+{
   uint8_t adc;
   adc = DEBUG_IN;
   if (adc == ' ')
@@ -165,13 +215,13 @@ ISR(TIMER2_COMPA_vect)
     _time_passed = 0u;
   }
 }
-
+*/
 uint32_t C_timestamp()
 {
   uint32_t ret;
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-    ret = _timestamp;
+    ret = 0;
   }
   return ret;  
 }
